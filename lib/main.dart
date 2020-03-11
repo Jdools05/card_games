4afdbfdaf7de21a39c4395d06969331b31dd74b7
Dillon Shaffer <46535284+Molkars@users.noreply.github.com>
2020-03-10 22:34:33 -0500

    Updated main.dart with good dart practices

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,31 @@
 import 'dart:math';
-
-import 'package:cardgames/playingCard.dart' as prefix0;
+import 'package:cardgames/playingCard.dart'; // Imports should always use the package:cardgames/dot_dot_dot.dart not the dot_dot_dot.dart format
 import 'package:flutter/material.dart';
-
-import 'playingCard.dart';
 
 void main() => runApp(MyApp());
 
-List<PlayingCard> allCards = [];
+// Avoid global variables, everything should be in the widget tree
+@deprecated List<PlayingCard> allCards = [];
 
+@immutable // Variables in the class can be changed, that being said they
+           // shouldn't and their changes don't cause the widget tree to rebuild
 class MyApp extends StatelessWidget {
-
 
   @override
   Widget build(BuildContext context) {
     populateCards();
     return MaterialApp(
       title: 'Card Games',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        primaryColorDark: Colors.grey,
-      ),
+      theme: ThemeData.light().copyWith(primaryColor: Colors.green, primaryAccentColor: Colors.greenAccent),
+      darkTheme: ThemeData.dark().copyWith(primaryColor: Colors.green, primaryAccentColor: Colors.greenAccent),
       home: MyHomePage(title: 'Card Games'),
     );
   }
 
   void populateCards() {
-    CardSuit.values.forEach((suit){
+    CardSuit.values.forEach((suit) {
       CardType.values.forEach((type) {
-        allCards.add(new PlayingCard(
+        allCards.add(PlayingCard( // The new keyword is not required as of Dart 2.0
           cardType: type,
           cardSuit: suit,
           faceUp: false,
@@ -36,8 +33,10 @@ class MyApp extends StatelessWidget {
       });
     });
   }
+  
 }
 
+@immutable
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -48,8 +47,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  int rand = 0;
+  int _counter = 0; // Make these private unless they are useful and doesn't break anything outside the class
+  int _rand = 0; // In dart _x is private and x is public
 
   void _incrementCounter() {
     setState(() {
@@ -72,10 +71,9 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 Image.asset('assets/2C.png', scale: 8),
                 Text(
-                  'This is your card: ${allCards[rand].cardType} of ${allCards[rand].cardSuit}',
-                ),
-                Text(
-                  'This is my first time using Flutter',
+                  // ''' makes it a multi line string
+                  '''This is your card: ${allCards[rand].cardType} of ${allCards[rand].cardSuit}
+                  This is my first time using Flutter''',
                 ),
                 Text(
                   'You have pushed the button this many times:',
